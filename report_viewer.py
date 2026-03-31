647ddd5e18b0d7c11c6b0951632b8767795d23ab
velkrosmaak <velkrosmaak@gmail.com>
2026-03-31 23:14:38 +0100

    listening host

--- report_viewer.py
+++ report_viewer.py
@@ -163,4 +163,4 @@ if __name__ == '__main__':
     os.makedirs('templates', exist_ok=True)
     os.makedirs('static/css', exist_ok=True)
     os.makedirs('static/js', exist_ok=True)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
